bd042d405a65292b1ecb81f134a653cb8c2bf45e
DiscreteTom <discrete_tom@outlook.com>
2024-02-01 22:39:33 +0800

    feat: finish simple web

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,47 +1,73 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <input type="text" v-model="pid" placeholder="pid" style="display: block" />
+    <button @click="ws === null ? connect() : disconnect()">
+      {{ ws === null ? 'Connect WebSocket' : 'Disconnect' }}
+    </button>
+    <button @click="add" :disabled="ws === null">Add</button>
+    <button @click="remove" :disabled="ws === null">Remove</button>
+    <button @click="removeAll" :disabled="ws === null">Remove All</button>
+    <button @click="toggle" :disabled="ws === null">{{ started ? 'Stop' : 'Start' }}</button>
+    <label>X</label>
+    <input type="range" min="-500" max="500" v-model="x" @input="update" :disabled="ws === null" />
+    <label>Y</label>
+    <input type="range" min="-500" max="500" v-model="y" @input="update" :disabled="ws === null" />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup lang="ts">
+import { ref } from 'vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const pid = ref('')
+const started = ref(false)
+const ws = ref<null | WebSocket>(null)
+const x = ref(0)
+const y = ref(0)
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+function connect() {
+  if (ws.value === null) {
+    // ws.value = new WebSocket(`ws://${window.location.host}`)
+    ws.value = new WebSocket(`ws://127.0.0.1:9002`)
+    ws.value.onopen = () => {
+      console.log('connected')
+    }
+    ws.value.onmessage = (e) => {
+      console.log('message', e.data)
+    }
+    ws.value.onclose = () => {
+      console.log('disconnected')
+      ws.value = null
+    }
   }
 }
-</style>
+function disconnect() {
+  if (ws.value !== null) {
+    ws.value.close()
+  }
+}
+
+function toggle() {
+  started.value = !started.value
+  ws.value?.send(JSON.stringify({ type: started.value ? 'start' : 'stop' }))
+}
+
+function add() {
+  console.log('add', pid.value)
+  ws.value?.send(JSON.stringify({ type: 'add', pid: Number(pid.value) }))
+}
+
+function remove() {
+  console.log('remove', pid.value)
+  ws.value?.send(JSON.stringify({ type: 'remove', pid: Number(pid.value) }))
+}
+
+function removeAll() {
+  console.log('remove all')
+  ws.value?.send(JSON.stringify({ type: 'removeAll' }))
+}
+
+function update() {
+  console.log('update', x.value, y.value)
+  ws.value?.send(JSON.stringify({ type: 'update', x: Number(x.value), y: Number(y.value) }))
+}
+</script>
