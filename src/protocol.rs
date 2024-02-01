140c6427ff82037e5125d233a3ff26c7eb47f624
DiscreteTom <discrete_tom@outlook.com>
2024-02-01 20:46:33 +0800

    chore: more actions

--- src/protocol.rs
+++ src/protocol.rs
@@ -7,8 +7,23 @@ pub struct UpdatePayload {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PidPayload {
+  pub pid: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
 pub enum Action {
+  #[serde(rename = "start")]
+  Start,
+  #[serde(rename = "stop")]
+  Stop,
+  #[serde(rename = "add")]
+  Add(PidPayload),
+  #[serde(rename = "remove")]
+  Remove(PidPayload),
+  #[serde(rename = "removeAll")]
+  RemoveAll,
   #[serde(rename = "update")]
   Update(UpdatePayload),
 }
