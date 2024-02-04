dd026f4e7d823844b43b4d46d2569f32eebd263f
DiscreteTom <discrete_tom@outlook.com>
2024-02-04 19:13:49 +0800

    refactor: split into manager and server module

---
+++ src/manager.rs
@@ -0,0 +1,172 @@
+use crate::protocol::{Action, PidsPayload, Reply};
+use tokio::sync::{mpsc, watch};
+use windows::Win32::{
+  Foundation::{BOOL, HWND, LPARAM, RECT},
+  UI::WindowsAndMessaging::{EnumWindows, GetWindowRect, GetWindowThreadProcessId, MoveWindow},
+};
+
+#[derive(Debug)]
+struct WindowState {
+  pub hwnd: HWND,
+  pub pid: u32,
+  pub x: i32,
+  pub y: i32,
+}
+
+static mut MANAGED_WINDOWS: Vec<WindowState> = Vec::new();
+static mut STARTED: bool = false;
+
+// https://learn.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms633498(v=vs.85)
+extern "system" fn enum_windows_callback(hwnd: HWND, l_param: LPARAM) -> BOOL {
+  let pid = l_param.0 as u32; // the target pid
+  let mut hwnd_pid = 0; // pid for the hwnd
+  unsafe {
+    // get pid by hwnd
+    GetWindowThreadProcessId(hwnd, Some(&mut hwnd_pid));
+  }
+  if hwnd_pid == pid {
+    unsafe {
+      let mut rect = RECT::default();
+      match GetWindowRect(hwnd, &mut rect) {
+        Ok(_) => {
+          // ensure the window is not already managed
+          if MANAGED_WINDOWS.iter().any(|w| w.hwnd == hwnd) {
+            return BOOL(0); // return false to stop enumerating
+          }
+
+          let state = WindowState {
+            hwnd,
+            // record the initial position
+            x: rect.left,
+            y: rect.top,
+            pid,
+          };
+          println!("Added window: {:?}", state);
+          MANAGED_WINDOWS.push(state);
+        }
+        Err(e) => eprintln!(
+          "Error getting window rect for pid({hwnd_pid}) and hwnd({}): {e:?}",
+          hwnd.0
+        ),
+      }
+    }
+    return BOOL(0); // return false to stop enumerating
+  }
+  return BOOL(1); // return true to continue enumerating
+}
+
+pub async fn start_manager(mut action_rx: mpsc::Receiver<Action>, reply_tx: watch::Sender<Reply>) {
+  loop {
+    match action_rx.recv().await {
+      None => break,
+      Some(action) => match action {
+        Action::Start => unsafe {
+          STARTED = true;
+          println!("Started");
+          if let Err(e) = reply_tx.send(Reply::Started) {
+            eprintln!("Error sending started reply: {e:?}");
+            break;
+          }
+        },
+        Action::Stop => unsafe {
+          STARTED = false;
+          println!("Stopped");
+          if let Err(e) = reply_tx.send(Reply::Stopped) {
+            eprintln!("Error sending stopped reply: {e:?}");
+            break;
+          }
+        },
+        Action::Add(pid_payload) => unsafe {
+          // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-enumwindows
+          match EnumWindows(
+            Some(enum_windows_callback),
+            LPARAM(pid_payload.pid as isize),
+          ) {
+            Ok(_) => (),
+            Err(e) => {
+              // if the return code is 0, there is no error
+              if e.code().0 != 0 {
+                eprintln!(
+                  "Error enumerating windows for pid({}): {e:?}",
+                  pid_payload.pid
+                )
+              }
+            }
+          }
+          if let Err(e) = reply_tx.send(Reply::CurrentManagedPids(PidsPayload {
+            pids: MANAGED_WINDOWS.iter().map(|w| w.pid).collect(),
+          })) {
+            eprintln!("Error sending current managed pids reply: {e:?}");
+            break;
+          }
+        },
+        Action::Remove(pid_payload) => unsafe {
+          MANAGED_WINDOWS.retain(|w| w.pid != pid_payload.pid);
+          println!("Removed window for pid({})", pid_payload.pid);
+          if let Err(e) = reply_tx.send(Reply::CurrentManagedPids(PidsPayload {
+            pids: MANAGED_WINDOWS.iter().map(|w| w.pid).collect(),
+          })) {
+            eprintln!("Error sending current managed pids reply: {e:?}");
+            break;
+          }
+        },
+        Action::RemoveAll => unsafe {
+          MANAGED_WINDOWS.clear();
+          println!("Removed all windows");
+          if let Err(e) = reply_tx.send(Reply::CurrentManagedPids(PidsPayload {
+            pids: MANAGED_WINDOWS.iter().map(|w| w.pid).collect(),
+          })) {
+            eprintln!("Error sending current managed pids reply: {e:?}");
+            break;
+          }
+        },
+        Action::Update(offset) => unsafe {
+          if STARTED {
+            let mut to_be_removed = Vec::new();
+            for w in &MANAGED_WINDOWS {
+              let mut rect = RECT::default();
+              match GetWindowRect(w.hwnd, &mut rect) {
+                Err(e) => {
+                  eprintln!(
+                    "Error getting window rect for pid({}), remove it. Error: {e:?}",
+                    w.pid
+                  );
+                  to_be_removed.push(w.pid);
+                  continue; // update next window
+                }
+                Ok(_) => (),
+              }
+              if let Err(e) = MoveWindow(
+                w.hwnd,
+                // use relative offset
+                offset.x + w.x,
+                offset.y + w.y,
+                // keep original size
+                rect.right - rect.left,
+                rect.bottom - rect.top,
+                false,
+              ) {
+                eprintln!(
+                  "Error setting window pos for pid({}), remove it. Error: {e:?}",
+                  w.pid
+                );
+                to_be_removed.push(w.pid);
+              }
+            }
+
+            // remove the windows that failed to update
+            if to_be_removed.len() > 0 {
+              MANAGED_WINDOWS.retain(|w| !to_be_removed.contains(&w.pid));
+              if let Err(e) = reply_tx.send(Reply::CurrentManagedPids(PidsPayload {
+                pids: MANAGED_WINDOWS.iter().map(|w| w.pid).collect(),
+              })) {
+                eprintln!("Error sending current managed pids reply: {e:?}");
+                break;
+              }
+            }
+          }
+        },
+      },
+    }
+  }
+}
---
+++ src/server.rs
@@ -0,0 +1,180 @@
+use crate::protocol::{Action, Reply};
+use futures_util::{SinkExt, StreamExt};
+use http_body_util::Full;
+use hyper::{
+  body::{Bytes, Incoming},
+  server::conn::http1,
+  upgrade::Upgraded,
+  Request, Response,
+};
+use hyper_tungstenite::HyperWebsocket;
+use hyper_util::rt::TokioIo;
+use serde_json;
+use std::net::SocketAddr;
+use tokio::{
+  net::TcpListener,
+  sync::{mpsc, watch},
+};
+use tokio_tungstenite::{
+  tungstenite::{Error, Message, Result},
+  WebSocketStream,
+};
+
+/// Handle a HTTP or WebSocket request.
+async fn handle_request(
+  mut request: Request<Incoming>,
+  peer: SocketAddr,
+  action_tx: mpsc::Sender<Action>,
+  reply_rx: watch::Receiver<Reply>,
+) -> Result<Response<Full<Bytes>>, Error> {
+  // Check if the request is a websocket upgrade request.
+  if hyper_tungstenite::is_upgrade_request(&request) {
+    let (response, websocket) = hyper_tungstenite::upgrade(&mut request, None)?;
+
+    // Spawn a task to handle the websocket connection.
+    tokio::spawn(async move {
+      if let Err(e) = serve_websocket(websocket, peer, action_tx, reply_rx).await {
+        eprintln!("Error in websocket connection: {e:?}");
+      }
+    });
+
+    // Return the response so the spawned future can continue.
+    Ok(response)
+  } else {
+    // Handle regular HTTP requests here.
+    Ok(Response::new(Full::<Bytes>::from("Hello HTTP!")))
+  }
+}
+
+async fn reply(ws: &mut WebSocketStream<TokioIo<Upgraded>>, reply: Reply) -> Result<(), ()> {
+  match ws
+    .send(Message::Text(match serde_json::to_string(&reply) {
+      Ok(s) => s,
+      Err(e) => {
+        eprintln!("Error serializing reply: {e:?}");
+        return Err(());
+      }
+    }))
+    .await
+  {
+    Ok(_) => (),
+    Err(e) => {
+      eprintln!("Error sending reply: {e:?}");
+      return Err(());
+    }
+  }
+  Ok(())
+}
+
+async fn serve_websocket(
+  ws: HyperWebsocket,
+  peer: SocketAddr,
+  action_tx: mpsc::Sender<Action>,
+  mut reply_rx: watch::Receiver<Reply>,
+) -> Result<()> {
+  let mut ws = ws.await?;
+  // let mut init_failed = false;
+
+  println!("WebSocket Connected: {peer}");
+
+  // TODO: init state
+  // if let Err(_) = reply(
+  //   &mut ws,
+  //   if unsafe { STARTED } {
+  //     Reply::Started
+  //   } else {
+  //     Reply::Stopped
+  //   },
+  // )
+  // .await
+  // {
+  //   init_failed = true;
+  // }
+
+  // unsafe {
+  //   if !init_failed {
+  //     if let Err(_) = reply(
+  //       &mut ws,
+  //       Reply::CurrentManagedPids(PidsPayload {
+  //         pids: MANAGED_WINDOWS.iter().map(|w| w.pid).collect(),
+  //       }),
+  //     )
+  //     .await
+  //     {
+  //       init_failed = true;
+  //     }
+  //   }
+  // }
+
+  // if !init_failed {
+
+  loop {
+    tokio::select! {
+      changed = reply_rx.changed() => {
+        if let Err(e) = changed {
+          eprintln!("Error receiving reply: {e:?}");
+          break;
+        }
+        let rep = reply_rx.borrow_and_update().clone();
+        if let Err(e) = reply(&mut ws, rep).await {
+          eprintln!("Error sending reply: {e:?}");
+          break;
+        }
+      }
+      msg = ws.next() => {
+        match msg {
+          None => break,
+          Some(msg) => match msg {
+            Err(e) => {
+              eprintln!("Error receiving message: {e:?}");
+              break;
+            }
+            Ok(msg) => match msg {
+              Message::Text(text) => {
+                let action: Action = serde_json::from_str(&text).unwrap();
+                action_tx.send(action).await.unwrap();
+              }
+              // other websocket message types are ignored
+              _ => (),
+            }
+          }
+        }
+      }
+    }
+  }
+
+  println!("WebSocket Disconnected: {}", peer);
+  Ok(())
+}
+
+pub async fn start_server(
+  addr: String,
+  action_tx: mpsc::Sender<Action>,
+  reply_rx: watch::Receiver<Reply>,
+) {
+  let listener = TcpListener::bind(&addr).await.expect("Can't listen");
+  let mut http = http1::Builder::new();
+  http.keep_alive(true);
+
+  println!("Listening on: {addr}");
+
+  loop {
+    let (stream, peer) = listener.accept().await.expect("failed to accept");
+    let action_tx = action_tx.clone();
+    let reply_rx = reply_rx.clone();
+    let connection = http
+      .serve_connection(
+        TokioIo::new(stream),
+        hyper::service::service_fn(move |req| {
+          handle_request(req, peer, action_tx.clone(), reply_rx.clone())
+        }),
+      )
+      .with_upgrades();
+
+    tokio::spawn(async move {
+      if let Err(err) = connection.await {
+        println!("Error serving HTTP connection: {err:?}");
+      }
+    });
+  }
+}
